75a6f23fe2f5d301f3d339884c10586642643b51
alekhyas98 <alekhyasangana98@gmail.com>
2025-10-06 20:04:35 +0530

    fix the bug

--- src/com/flm/ternary/Ternary.java
+++ src/com/flm/ternary/Ternary.java
@@ -4,7 +4,7 @@ package com.flm.ternary;
 
 public class Ternary {
 	public static void main(String[] args) {
-		int marks=41;
+		int marks=95;
 		String grade=marks>90?"Grade A":
 			marks>70 && marks<=90?"Grade B":
 				marks>50 && marks<=70 ? "Grade C":"Fail";
